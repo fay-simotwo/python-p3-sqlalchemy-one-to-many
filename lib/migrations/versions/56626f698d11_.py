45eacb069864ee6f6072c752cc20d492756a35bc
Moringa School <chepfaith18@gmail.com>
2023-09-03 15:12:46 +0300

    completed assignment

---
+++ lib/migrations/versions/56626f698d11_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 56626f698d11
+Revises: faebd12f9523
+Create Date: 2023-09-03 14:21:57.551650
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '56626f698d11'
+down_revision = 'faebd12f9523'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
